bb8dfd5f1e97a43ed594beda57a41f2ddf3e88ca
Bujar Sefa <bujarsefa9@gmail.com>
2021-04-17 02:38:45 -0400

    Made changes to add JUMP

--- SEFA_Branching_MUX.vhd
+++ SEFA_Branching_MUX.vhd
@@ -3,15 +3,16 @@ use IEEE.std_logic_1164.all;
 use work.SEFA_BRANCHING_PACKAGE.all;
 
 
--- The purpose of this component is to select between PC+4 or PC+SignExtended(Imm16)+4
+-- The purpose of this component is to select between PC+4 or PC+SignExtended(Imm16)+4 OR PC+SIGNEXTENDED(IMM26)+4
 -- It must call the appropriate components that perform the addtion.
 
 -- note putting this on pause. as this can be done directly in one file. 
 
 ENTITY SEFA_Branching_MUX IS
 	PORT(
+		SEFA_OPCODE : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
 		SEFA_PC_Plus_4 : IN STD_LOGIC_VECTOR(31 DOWNTo 0);
-		SEFA_PC_IMM16_Plus_4 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+		SEFA_PC_IMM_Plus_4 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 		SEFA_PC_SELECTOR : IN STD_LOGIC; 
 		SEFA_PC_NEW : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
 	);
@@ -19,7 +20,7 @@ END SEFA_Branching_MUX;
 
 ARCHITECTURE arch OF SEFA_Branching_MUX IS
 BEGIN
-	SEFA_PC_NEW <= SEFA_PC_IMM16_Plus_4 WHEN SEFA_PC_SELECTOR = '1'
+	SEFA_PC_NEW <= SEFA_PC_IMM_Plus_4 WHEN (SEFA_OPCODE = "111111" OR SEFA_PC_SELECTOR = '1')
 							ELSE SEFA_PC_Plus_4;
 
 END arch;
--- SEFA_IR_REGISTER_DRIVER.vhd
+++ SEFA_IR_REGISTER_DRIVER.vhd
@@ -8,85 +8,47 @@ use work.SEFA_BRANCHING_PACKAGE.all;
 -- we also might be able to avoid this file and do ir directly in IR_register
 -- but im using ir register just for the sake of storing (read/write) data
 
-
-
 ENTITY SEFA_IR_REGISTER_DRIVER IS
 PORT (
---		SEFA_OPCODE : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
---		SEFA_RS_REGISTER_ADDRESS : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
---		SEFA_RT_REGISTER_ADDRESS : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
---		SEFA_IMMEDIATE16_VALUE : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-		
-		-- 
-		SEFA_clk : in std_logic; 
---		SEFA_wren: in std_logic; -- write enable (if it is 0, the stored data will not change)
---		SEFA_rden: in std_logic; -- read enable (only when it is 1, the stored data will be displayed to output)
---		SEFA_chen: in std_logic; --  chip enable (if it is 0, the output will be undefined)
---		
-		
-		
---		SEFA_IR_REGISTER_VALUE : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-
-
-		SEFA_PC_NEW : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) -- NOT SURE WHAT THE OUTPUT IS SUPPOSED TO BE. BUT WE CAN OUTPUT THIS. 
+	SEFA_clk: in std_logic;
+	SEFA_IR_REGISTER_VALUE : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+	 SEFA_OPCODE : OUT  STD_LOGIC_VECTOR(5 DOWNTO 0);
+	 SEFA_RS_REGISTER_ADDRESS : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+	 SEFA_RT_REGISTER_ADDRESS : OUT  STD_LOGIC_VECTOR(4 DOWNTO 0);
+	 SEFA_IMMEDIATE16_VALUE : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+	 SEFA_IMMEDIATE26_VALUE : OUT STD_LOGIC_VECTOR(25 DOWNTO 0)
 );
 END SEFA_IR_REGISTER_DRIVER;
 
 ARCHITECTURE arch OF SEFA_IR_REGISTER_DRIVER IS
-
-	SIGNAL SEFA_IR_VALUE : STD_LOGIC_VECTOR(31 DOWNTO 0);
-	SIGNAL SEFA_OPCODE : STD_LOGIC_VECTOR(5 DOWNTO 0);
-	SIGNAL SEFA_RS_REGISTER_ADDRESS : STD_LOGIC_VECTOR(4 DOWNTO 0);
-	SIGNAL SEFA_RT_REGISTER_ADDRESS : STD_LOGIC_VECTOR(4 DOWNTO 0);
-	SIGNAL SEFA_IMMEDIATE16_VALUE : STD_LOGIC_VECTOR(15 DOWNTO 0);
+	SIGNAL SEFA_IR_REGISTER_VALUE_OUTPUT: STD_LOGIC_VECTOR(31 DOWNTO 0);
 
 BEGIN
 
-	-- Set the values by default. Ensure that the register rs/rt values in MIF to be equal (if you want to test the condition). 
-	-- Might be easier to just pass in these values so that we can pass them through the test bench....
-	-- NOTE BY DEFAULT SETTING PC ADDRESS TO "00000" -- THIS MUST BE CONSISTENT EVERYWHERE! 
-	SEFA_OPCODE <= "000000";
-	SEFA_RS_REGISTER_ADDRESS <= "00010";
-	SEFA_RT_REGISTER_ADDRESS <= "00001";
-	SEFA_IMMEDIATE16_VALUE <= "0000000000001000"; 
 
-	-- Set the combined value that is passsed into driver. 
-	SEFA_IR_VALUE <= SEFA_OPCODE & SEFA_RS_REGISTER_ADDRESS & SEFA_RT_REGISTER_ADDRESS & SEFA_IMMEDIATE16_VALUE; 
+	SET_AND_GET_IR_VALUE_IN_REGISTER : SEFA_IR_REGISTER port map (
+		SEFA_clk => SEFA_clk, 
+		SEFA_wren => '1',
+		SEFA_rden => '1', 
+		SEFA_chen => '1', 
+		SEFA_data => SEFA_IR_REGISTER_VALUE, 
+		SEFA_IR=> SEFA_IR_REGISTER_VALUE_OUTPUT
+	);
+	
 
-	-- we manually set the code for bne, beq
-	
---	
---	COMPUTE_NEXT_PC : SEFA_COMPUTE_NAL_FROM_IR_VAL PORT MAP (
---		SEFA_IR_REGISTER_VALUE => SEFA_IR_VALUE,
---		SEFA_clk => SEFA_clk, 
---		SEFA_PC_NEW => SEFA_PC_NEW
---	
---	
---	
---	);
+	SEFA_OPCODE <= SEFA_IR_REGISTER_VALUE_OUTPUT(31 DOWNTO 26);
+	SEFA_RS_REGISTER_ADDRESS <= SEFA_IR_REGISTER_VALUE_OUTPUT(25 DOWNTO 21);
+	SEFA_RT_REGISTER_ADDRESS <= SEFA_IR_REGISTER_VALUE_OUTPUT(20 DOWNTO 16);
+	SEFA_IMMEDIATE16_VALUE <= SEFA_IR_REGISTER_VALUE_OUTPUT(15 DOWNTO 0);
 	
 	
 	
---	
---		SEFA_IR_REGISTER_VALUE : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
---	SEFA_clk : IN STD_LOGIC;
-----	SEFA_wren: in std_logic; -- write enable (if it is 0, the stored data will not change)
-----	SEFA_rden: in std_logic; -- read enable (only when it is 1, the stored data will be displayed to output)
-----	SEFA_chen: in std_logic; --  chip enable (if it is 0, the output will be undefined)
---	
---	SEFA_PC_NEW : OUT STD_LOGIC_VECTOR(31 DOWNTO 0); -- uSING THIS FOR NOW B/C
---	-- IM NOT SURE WHAT THE OUTPUT IS SUPPOSED TO BE. 
+	-- NOTE. WHEN JUMP IS EXECUTING, THE RESUT OF THE VALUES WILL HAVE "JUNK" 
+	-- WHEN EXECUTING RS RT IMM16, WELL THOSE ARE SIMPLY STORED IN IMM26 (BUT WE WONT USE IT)
+	SEFA_IMMEDIATE26_VALUE <= SEFA_IR_REGISTER_VALUE_OUTPUT(25 DOWNTO 0);
+
 	
-	
-	--	IR_REGISTER: sEFA_IR_REGISTER PORT MAP (
---		SEFA_clk => SEFA_clk,
---		SEFA_wren => SEFA_wren,
---		SEFA_rden => SEFA_rden,
---		SEFA_chen => SEFA_chen, 
---		SEFA_data => SEFA_IR_VALUE,
---		SEFA_IR => SEFA_IR_REGISTER_VALUE
---
---		);
+
 
 END arch;
 
